86856393e3f3da1c715904ebf1ec16bf402219cf
“aretaneilaputri <retaaneila@gmail.com>
2026-03-05 20:32:54 +0700

    3.5 — Tugas (Dosen02.java)

---
+++ Dosen02.java
@@ -0,0 +1,13 @@
+public class Dosen02 {
+    public String kode;
+    public String nama;
+    public Boolean jenisKelamin;
+    public int usia;
+
+    public Dosen02(String kode, String nama, Boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+}
