458adfb72dfc4f9a3008cd0559cef57ea0450165
“aretaneilaputri <retaaneila@gmail.com>
2026-03-05 20:22:02 +0700

    Modifikasi 3.4 — Pertanyaan nomor 4 (MatakuliahDemo02.java)

--- MatakuliahDemo02.java
+++ MatakuliahDemo02.java
@@ -1,34 +1,49 @@
 import java.util.Scanner;
-public class MatakuliahDemo02 {
 
+public class MatakuliahDemo02 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        Matakuliah02[] arrayOfMatakuliah = new Matakuliah02[3];
+
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for (int i = 0; i < 3; i++) {
+        System.out.print("Masukkan jumlah Matakuliah : ");
+        int jumlah = Integer.parseInt(sc.nextLine());
+
+        Matakuliah02[] arrayOfMatakuliah = new Matakuliah02[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
+
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
-            System.out.print( "Kode       : ");
+
+            System.out.print("Kode        : ");
             kode = sc.nextLine();
-            System.out.print( "Nama     : ");
+
+            System.out.print("Nama        : ");
             nama = sc.nextLine();
-            System.out.print( "Sks      : ");
+
+            System.out.print("SKS         : ");
             dummy = sc.nextLine();
             sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam : ");
+
+            System.out.print("Jumlah Jam  : ");
             dummy = sc.nextLine();
             jumlahJam = Integer.parseInt(dummy);
-            System.out.println("--------------------------------------------");
+
+            System.out.println("------------------------------------");
 
             arrayOfMatakuliah[i] = new Matakuliah02();
             arrayOfMatakuliah[i].tambahData(kode, nama, sks, jumlahJam);
         }
 
-        for(int i = 0; i < 3; i++) {
+        
+
+        for (int i = 0; i < jumlah; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah[i].cetakInfo();
+        }
+
+        sc.close();
     }
-    }
-    
 }
